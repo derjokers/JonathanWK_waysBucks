460c9f5cf649fcd531a433a80928a636daf4866e
derjokers <karongkongjonathan@gmail.com>
2021-08-24 07:50:39 +0800

    add transaction page

---
+++ src/Pages/Admin.js
@@ -0,0 +1,46 @@
+import { Table, Button } from "react-bootstrap";
+import NavbarWaysBucks from "../Components/NavbarWaysbucks";
+
+import '../Styles/Admin.css'
+
+export default function Admin(props) {
+    return (
+        <div className="admin">
+            <NavbarWaysBucks />
+            <div className="adminTransaction">
+                <h2>Income Transaction</h2>
+                <Table className='mt-4' bordered hover>
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Name</th>
+                            <th>Address</th>
+                            <th>Post Code</th>
+                            <th>Income</th>
+                            <th>Status</th>
+                            <th className="action">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                            <td>1</td>
+                            <td>Sugeng No Pants</td>
+                            <td>Cileungsi</td>
+                            <td>16820</td>
+                            <td>69.000</td>
+                            <td>Waiting Approve</td>
+                            <td className='d-flex justify-content-evenly'>
+                                <Button variant='danger' size='sm'>
+                                    Cancel
+                                </Button>
+                                <Button variant='success' size='sm'>
+                                    Approve
+                                </Button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import DetailProduct from './Pages/DetailProduct';
+import Landing from './Pages/Landing';
+import Admin from './Pages/Admin';
 
 ReactDOM.render(
   <React.StrictMode>
-    <DetailProduct />
+    <Admin />
   </React.StrictMode>,
   document.getElementById('root')
 );
